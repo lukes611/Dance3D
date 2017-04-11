1c83346b1336b16089f3b5f1268f7ee3a4996fd7
luke <lukes611@gmail.com>
2017-04-11 11:42:03 +1000

    improving design in LSkeleton

--- static/js/loqur/LSkeleton.js
+++ static/js/loqur/LSkeleton.js
@@ -1,33 +1,47 @@
 
-
-function LBone(id, parent, baseSize, length, location, rotation){
+//create a new bont: qt is optional
+function LBone(id, parent, baseSize, length, location, qt){
   this.id = id;
   this.parent = parent;
   this.location = location.copy();
-  this.rotation = rotation.copy();
+  if(qt === undefined) this.qt = LQt.rotation(0,0,0);
+  else this.qt = qt.copy();
   this.length = length;
   this.base = baseSize;
 }
 
+//create deep copy of bone (excludes parent linked list)
 LBone.prototype.copy = function(){
-  return new LBone(this.id, this.parent, this.base, this.length, this.location, this.rotation);
+  return new LBone(this.id, this.parent, this.base, this.length, this.location, this.qt);
 };
 
+//convert bone to string representation
 LBone.prototype.toString = function(){
-  return JSON.stringify({id:this.id, location:this.location,rotation:this.rotation,length:this.length,base:this.base});
+  return JSON.stringify({id:this.id, location:this.location,qt:this.qt,length:this.length,base:this.base});
 };
 
+//convert string representation to bone
 LBone.fromString = function(s, parent){
   parent = parent || null;
   var ob = JSON.parse(s);
-  return new LBone(ob.id, parent, ob.base, ob.length, LV3.fromJSON(ob.location), LV3.fromJSON(ob.rotation));
+  return new LBone(ob.id, parent, ob.base, ob.length, LV3.fromJSON(ob.location), LV3.fromJSON(ob.qt));
 };
 
-LBone.prototype.qt = function(){
-  if(this.parent == null) return LQt.rotation(this.rotation.x, this.rotation.y, this.rotation.z);
+//rotate the bone by x,y,z
+LBone.prototype.rotate = function(x,y,z){
+  var rotation = LQt.rotation(x,y,z);
+  this.qt = rotation.mult(this.qt);
+};
+
+LBone.prototype.getQt = function(){
+  return this.qt.copy();
+};
+
+//get the compounded qt
+LBone.prototype.cqt = function(){
+  if(this.parent == null) return this.getQt();
   else{
-    var myQt = LQt.rotation(this.rotation.x, this.rotation.y, this.rotation.z);
-    return this.parent.qt().mult(myQt);
+    return this.parent.cqt().mult(this.getQt());
   }
 };
 
@@ -35,16 +49,16 @@ LBone.prototype.tip = function(){
   var p, myQt;
   if(this.parent == null){ //for the base, the tip is just the end + the base (location)
     p = new LV3(this.length, 0, 0);
-    myQt = LQt.rotation(this.rotation.x, this.rotation.y, this.rotation.z);
+    myQt = this.getQt();
     myQt.multLV3(p);
     return p.add(this.location);
   }else{ //otherwise, it is the other tip,
     p = new LV3(this.length, 0, 0);
 
-    myQt = this.qt()
+    myQt = this.cqt();
     myQt.multLV3(p);
     var l = this.location.copy();
-    var qt2 = this.parent.qt();
+    var qt2 = this.parent.cqt();
     qt2.multLV3(l);
     return p.add(this.parent.tip()).add(l);
   }
@@ -54,7 +68,7 @@ LBone.prototype.tip = function(){
 LBone.prototype.loc = function(){
   if(this.parent == null) return this.location.copy();
   else{
-    var pqt = this.parent.qt();
+    var pqt = this.parent.cqt();
     var l = this.location.copy();
     pqt.multLV3(l);
     return this.parent.tip().add(l);
@@ -108,7 +122,7 @@ LBone.prototype.getModel = function(){
   ret.faces.push([7,9,6]);
   ret.faces.push([3,9,7]);
 
-  var qt = this.qt();
+  var qt = this.cqt();
   var loc = this.loc();
   for(var i = 0; i < ret.verts.length; i++){
     qt.multLV3(ret.verts[i]);
@@ -118,4 +132,28 @@ LBone.prototype.getModel = function(){
 
   return ret;
 
+};
+
+
+function LSkeleton(){
+  this.bones = [];
+}
+
+LSkeleton.prototype.indexById = function(id){
+  for(var i = 0; i < this.bones.length; i++)
+    if(this.bones[i].id == id) return i;
+  return -1;
+};
+
+LSkeleton.prototype.byId = function(id){
+  for(var i = 0; i < this.bones.length; i++)
+    if(this.bones[i].id == id) return this.bones[i];
+  return null;
+};
+
+LSkeleton.prototype.add = function(newBone, parent){
+  if(parent === undefined) parent = null;
+  if(typeof parent == 'Number' || typeof parent == 'string') parent = this.byId(parent);
+  if(parent != null) newBone.parent = parent;
+  this.bones.push(newBone);
 };
